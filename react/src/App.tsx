cc87424d84ca3db7b13244ee7669de9fd01889b0
CassioGuimaraes <131621517+CassioSobreira@users.noreply.github.com>
2025-09-10 16:23:36 -0300

    iniciando

---
+++ react/src/App.tsx
@@ -0,0 +1,12 @@
+function App() {
+  return(
+    <main>
+      <h1 className="text-4xl font-bold">Calculadora de Idade</h1>
+      <p>Insira sua data de nascimento:</p>
+      <input type="date" />
+      <button>Calcular</button>
+    </main>
+  )
+}
+
+export default App
